656c563371390f0c52c02cb884812a917480ec56
Mikael Ågren <mikael@tillitis.se>
2025-05-23 13:03:35 +0200

    tkey: Lower flash write RAM consumption
    Castor support 256 byte writes. Lowering write buffer size from 4096 to
    256 byte.

--- targets/tkey/src/flash.c
+++ targets/tkey/src/flash.c
@@ -14,7 +14,9 @@
 #include "log.h"
 #include "flash.h"
 
-const uint32_t FLASH_PAGE_ALIGN_MASK = (~(FLASH_PAGE_SIZE - 1));
+#define WRITE_SIZE 256
+
+const uint32_t WRITE_ALIGN_MASK = (~(WRITE_SIZE - 1));
 
 void flash_erase_page(uint8_t page)
 {
@@ -35,9 +37,9 @@ void flash_write_dword(uint32_t addr, uint64_t data)
 
 void flash_write(uint32_t addr, uint8_t * data, size_t sz)
 {
-    uint8_t buf[FLASH_PAGE_SIZE];
+    uint8_t buf[WRITE_SIZE];
 
-    uint32_t buf_offset = addr & ~FLASH_PAGE_ALIGN_MASK;
+    uint32_t buf_offset = addr & ~WRITE_ALIGN_MASK;
     size_t len = 0;
 
     printf2(TAG_GREEN,"flash write addr=%lx, size=%lx\r\n", addr, sz);
@@ -45,14 +47,14 @@ void flash_write(uint32_t addr, uint8_t * data, size_t sz)
     for(size_t i = 0; i < sz; i += len)
     {
         size_t bytes_left = sz - i;
-        size_t dst_max_len = FLASH_PAGE_SIZE - buf_offset;
-        size_t src_max_len = bytes_left < FLASH_PAGE_SIZE ? bytes_left : FLASH_PAGE_SIZE;
+        size_t dst_max_len = WRITE_SIZE - buf_offset;
+        size_t src_max_len = bytes_left < WRITE_SIZE ? bytes_left : WRITE_SIZE;
         len = src_max_len < dst_max_len ? src_max_len : dst_max_len;
 
         memset(buf, 0xff, sizeof(buf));
         memcpy(buf + buf_offset, data + i, len);
 
-        int ret = sys_write(addr & FLASH_PAGE_ALIGN_MASK, buf, FLASH_PAGE_SIZE);
+        int ret = sys_write(addr & WRITE_ALIGN_MASK, buf, WRITE_SIZE);
         if (ret != 0) {
             printf2(TAG_ERR,"flash write NOT successful addr=%lx, ret=%lx\r\n", addr, ret);
         }
