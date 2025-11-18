f19b57ecfe8178a598eb261eb4df5cbc93a17702
Daniel Jobson <jobson@tillitis.se>
2025-11-18 08:38:28 +0100

    Add build-tkey.sh to prepare for CI
    This will checkout tkey-libs and build it.
    NOTE: the script takes latest main, should be locked to an appropriate
    version once we have one.

---
+++ build-tkey.sh
@@ -0,0 +1,21 @@
+#! /bin/sh
+
+# SPDX-FileCopyrightText: 2023 Tillitis AB <tillitis.se>
+# SPDX-License-Identifier: BSD-2-Clause
+
+set -eu
+
+tkey_libs_version="main"
+
+printf "Building tkey-libs with version: %s\n" "$tkey_libs_version"
+
+if [ -d ../tkey-libs ]
+then
+    (cd ../tkey-libs; git checkout "$tkey_libs_version")
+else
+    git clone -b "$tkey_libs_version" https://github.com/tillitis/tkey-libs.git ../tkey-libs
+fi
+
+make -j -C ../tkey-libs
+
+make -j tkey_app
